c3a98f0881493ea6a5534a6b18ece387831adc61
Phil J. Laszkowicz <phil@fillip.pro>
2017-05-21 13:20:00 +0300

    Includes web components & Vue.

--- assets/scripts/src/eostre.ts
+++ assets/scripts/src/eostre.ts
@@ -3,6 +3,12 @@
 import * as log from "./util/logging";
 import * as analytics from "./util/analytics";
 
+import Vue from 'Vue';
+
 export class Eostre {
-    
+    constructor() {
+
+    }
 }
+
+let e = new Eostre();
